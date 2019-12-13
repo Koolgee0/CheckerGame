69d0eae4f0e3927dd0e9b4165e753639df1a2ceb
Koolgee0 <Koolgee100@aol.com>
2019-12-12 20:57:21 -0500

    Reload up

--- src/main/java/KEProject_1.java
+++ src/main/java/KEProject_1.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 //Ritika was here and so was glenn
 public class KEProject_1 {
     public static void main(String[] args) {
@@ -13,4 +13,46 @@ public class KEProject_1 {
     }
 }
 
+class Checker {
+    Scanner input = new Scanner(System.in);
+    private Board gameboard;
+    private Player playerone;
+    private Player playertwo;
+
+    public Board getGameboard() {
+        return gameboard;
+    }
+
+    public Player getPlayerone() {
+        return playerone;
+    }
+
+    public Player getPlayertwo() {
+        return playertwo;
+    }
+
+    public void setUpByUser() {
+        System.out.println("Hello Welcome Player Here is were you choose if you want Computer or Human opponet");
+        System.out.println("Type Computer for Computer Player or Type Human for Human Player");
+        String choose = input.nextLine();
+        if (choose.equals("Computer")) {
+            playertwo = new Computer("Black");
+        } else System.out.println("Human");
+        playertwo= new Human("Black");
+    }
+
+    public boolean isInProgress() {
+
+        return false;
+    }
+
+    public void currentPlayerTakeTurn() {
+    }
+
+    public void displayBoard() {
+    }
+
+    public void displayEndOfGameMessage() {
+    }
+}
 

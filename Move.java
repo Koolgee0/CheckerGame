bf801950c3dfb9a18653a1b899b26e1e90298279
Gelilea <58054357+Gelilea@users.noreply.github.com>
2019-12-30 19:12:37 -0500

    Add files via upload
    These are the Piece and Move files for the Checkers Game.

---
+++ Move.java
@@ -0,0 +1,17 @@
+package main.java;
+
+public class Move {
+    private int initialXCoor;
+    private int initialYCoor;
+    private int endingXCoor;
+    private int endingYCoor;
+    
+    Move(int initialXCoor, int initialYCoor, int endingXCoor, int endingYCoor) {
+    	this.initialXCoor = initialXCoor;
+    	this.initialYCoor = initialYCoor;
+    	this.endingXCoor = endingXCoor;
+    	this.endingYCoor = endingYCoor; 
+    	
+    }
+
+}

5e17762544c61b1ea3b4d7c2efc8afa9b75b68dc
Gelilea <58054357+Gelilea@users.noreply.github.com>
2020-01-06 13:10:36 -0500

    Add files
    Add Move, Coordinate, and Piece.

--- src/main/java/main/java/Move.java
+++ src/main/java/main/java/Move.java
@@ -1,9 +1,35 @@
 package main.java;
 
 public class Move {
-    private int initalXCoor;
-    private int initalYCoor;
+    private int initialXCoor;
+    private int initialYCoor;
     private int endingXCoor;
     private int endingYCoor;
+    
+    Move(int initialXCoor, int initialYCoor, int endingXCoor, int endingYCoor) {
+    	this.initialXCoor = initialXCoor;
+    	this.initialYCoor = initialYCoor;
+    	this.endingXCoor = endingXCoor;
+    	this.endingYCoor = endingYCoor; 
+    	
+    }
+    
+    public int getInitialXCoor() {
+        return initialXCoor;
+    }
 
+    public int getInitialYCoor() {
+        return initialYCoor;
+    }
+
+    public int getEndingXCoor() {
+        return endingXCoor;
+    }
+
+    public int getEndingYCoor() {
+        return endingYCoor;
+    }
 }
+
+
+

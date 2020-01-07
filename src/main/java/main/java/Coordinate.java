4a6a6859806464b5b996cb5b3465fdd5465a148a
Koolgee0 <Koolgee100@aol.com>
2020-01-07 16:45:29 -0500

    Reupload For Gelilea

--- src/main/java/main/java/Coordinate.java
+++ src/main/java/main/java/Coordinate.java
@@ -1,9 +1,20 @@
 package main.java;
 
 public class Coordinate {
-	
-	
+    private int xCoordinate;
+    private int yCoordinate;
 
-	
-	
+    Coordinate(int xCoordinate, int yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    public int getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public int getYCoordinate() {
+        return yCoordinate;
+    }
+
 }
